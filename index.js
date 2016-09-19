06629bafd12312b7a8af0d58f5b24eec26d69c9d
Martin Sarabia <martin@brightcoders.com>
2016-09-19 15:16:07 -0500

    Set initital configuratioins

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://parse:parse2016!@165.225.165.8:27017/parse',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'pidoTaxi',
+  masterKey: process.env.MASTER_KEY || 'qJAmfJCjhke5iw7E20wnJFFo22oKFT1EmZTxdq3j', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://parse-bc.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
